2695094106bbe724fb198383d8fb2cd3bf1316c5
Félix Doyon <felix.doyon@hotmail.com>
2025-12-22 15:24:30 -0500

    Update Protocol_Com_BLE_Manager.py

--- Protocol_Com_BLE_Manager.py
+++ Protocol_Com_BLE_Manager.py
@@ -10,6 +10,7 @@ import os
 import struct
 import sys
 import pigpio
+import traceback
 
 # UUIDs
 SERVICE_UUID = "0000180a-0000-1000-8000-00805f9b34fb"
@@ -287,4 +288,5 @@ if __name__ == "__main__":
         print("Interrupted by user")
     except Exception as e:
         print(f"Exception in main: {e}")
+        traceback.print_exc()
         sys.exit(1)
